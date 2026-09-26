169750f6f102ff5b15e60c88c1bac0162a93a471
isaac njoroge <isaackhanja1213@gmail.com>
2026-09-26 12:07:39 +0300

    Fix Render CSRF trusted origin

--- config/settings.py
+++ config/settings.py
@@ -59,6 +59,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
     "https://iscpooltracker.onrender.com",
+    "https://isc-pool-tracker.onrender.com",
     "https://isac1213.pythonanywhere.com",
 ]
 
